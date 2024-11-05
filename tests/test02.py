888b3c5c662b03f3865e40c85101ad94d94e52c5
zhuli <zhuli_wu@163.com>
2024-11-05 13:59:08 +0800

    branch-05: add test02.py

---
+++ tests/test02.py
@@ -0,0 +1,14 @@
+
+#! /usr/bin/env python3
+
+
+
+
+
+print('add test information1...')
+print('add test information2...')
+print('add test information3...')
+print('add test information4...')
+
+
+
